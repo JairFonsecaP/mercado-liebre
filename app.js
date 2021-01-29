7c63da7fc885b7280c698e785eb430baa819faff
JairFonsecaP <jairfonseca64@yahoo.esa>
2021-01-29 00:21:35 -0300

    actualice el orden

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const port = 3000;
 
+app.use("/static", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/home.html"));
 });
@@ -14,8 +16,6 @@ app.get("/register", (req, res) => {
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/login.html"));
 });
-
-app.use("/static", express.static(__dirname + "/public"));
 
 app.listen(process.env.PORT || port, function () {
   console.log("server up");
