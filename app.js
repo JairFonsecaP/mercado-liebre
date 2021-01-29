dffb203ea264630262d246cfdf2f704f64b0b62d
JairFonsecaP <jairfonseca64@yahoo.esa>
2021-01-28 23:19:15 -0300

    Mercado liebre terminado

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+
+app.use("/static", express.static(__dirname + "/public"));
+
+app.listen(port, () => {
+  console.log("server up at port " + port);
+});
